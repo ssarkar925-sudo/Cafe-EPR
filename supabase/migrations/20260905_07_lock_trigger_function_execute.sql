d573f5f7aacee9f42b36fa9747e8eeffa9d5110b
ssarkar925-sudo <ssarkar925@gmail.com>
2026-09-05 12:28:11 +0530

    security: hide internal transaction trigger functions

---
+++ supabase/migrations/20260905_07_lock_trigger_function_execute.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.trg_block_posted_transaction_financial_update() from public, anon, authenticated;
+revoke execute on function public.trg_block_posted_transaction_financial_delete() from public, anon, authenticated;
