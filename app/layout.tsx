3db22c85ff6d3439558325c6a74e98d1e0cf039c
ssarkar925-sudo <ssarkar925@gmail.com>
2026-09-12 18:58:37 +0530

    Load modern POS billing visual layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./mobile-modal-overrides.css";
 import "./receipt-visual-fixes.css";
 import "./quick-access.css";
 import "./pos-workspace.css";
+import "./pos-modern-billing.css";
 import ThemeProvider from "@/components/theme-provider";
 import { NotificationProvider } from "@/components/ui/notification-provider";
 
